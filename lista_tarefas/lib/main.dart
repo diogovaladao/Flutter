e62679b20c88a1149b2e29ba32d02d14ea26ecff
diogovaladao <diogo.svaladao@gmail.com>
2020-04-09 15:19:19 -0300

    excluindo intens

--- lista_tarefas/lib/main.dart
+++ lista_tarefas/lib/main.dart
@@ -20,6 +20,9 @@ class _HomeState extends State<Home> {
   final _toDoContrle = TextEditingController();
   List _toDoList = [];
 
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
   @override
   void initState() {
     super.initState();
@@ -91,7 +94,9 @@ class _HomeState extends State<Home> {
         color: Colors.red,
         child: Align(
           alignment: Alignment(-0.9, 0.0),
-          child: Icon(Icons.delete, color: Colors.white,
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
           ),
         ),
       ),
@@ -100,8 +105,7 @@ class _HomeState extends State<Home> {
         title: Text(_toDoList[index]["title"]),
         value: _toDoList[index]["ok"],
         secondary: CircleAvatar(
-          child: Icon(
-              _toDoList[index]["ok"] ? Icons.check : Icons.error),
+          child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
         ),
         onChanged: (c) {
           setState(() {
@@ -110,6 +114,30 @@ class _HomeState extends State<Home> {
           });
         },
       ),
+      // removendo item na lista
+      onDismissed: (direcao) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+          _saveData();
+
+          // desafazendo ação de exclusão da lista
+          final snack = SnackBar(
+            content: Text("Tarefa ${_lastRemoved["title"]} removida"),
+            action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: () {
+                  setState(() {
+                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+                }),
+            duration: Duration(seconds: 2),
+          );
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 
